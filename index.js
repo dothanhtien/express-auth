33b16cc1dcfe386439d0dff6ef1ca93c92b17c97
Tien Do Thanh <dothanhtien95@gmail.com>
2022-04-02 23:00:36 +0700

    configure to connect mysql using sequelize

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
+"use strict";
+
 const express = require("express");
+const { sequelize } = require("./models");
 
 const app = express();
 
 const PORT = process.env.PORT || 8080;
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Connection has been established successfully.");
+  })
+  .catch((error) => {
+    console.error("Unable to connect to the database:", error);
+  });
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
